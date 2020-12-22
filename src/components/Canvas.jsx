356f88addf78648475034f30a10861dfb93bdb71
Noel Pomaville <ntpomaville@gmail.com>
2020-12-22 10:49:36 -0500

    Implement movement of UFOs

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,13 +4,94 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CurrentScore from './CurrentScore'
+import FlyingObject from './FlyingObject';
+import StartGame from './StartGame';
+import Title from './Title';
+
+const Canvas = (props) => {
+  const gameHeight = 1200;
+  const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CurrentScore score={15} />
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
+
+      {/*{ props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }*/}
+
+      {props.gameState.flyingObjects.map(flyingObject => (
+        <FlyingObject
+          key={flyingObject.id}
+          position={flyingObject.position}
+        />
+      ))}
+    </svg>
+  );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+        position: PropTypes.shape({
+          x: PropTypes.number.isRequired,
+          y: PropTypes.number.isRequired
+        }).isRequired,
+        id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+};
+
+export default Canvas;
+
+
+
+/*import React from 'react';
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
+import Heart from './Heart';
+import StartGame from './StartGame';
+import Title from './Title';
 
 const Canvas = (props) => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-  return (
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];  return (
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
@@ -30,6 +111,9 @@ const Canvas = (props) => {
       <CurrentScore score={15} />
       <FlyingObject position={{x: -150, y: -300}}/>
       <FlyingObject position={{x: 150, y: -300}}/>
+      <Heart position={{x: -300, y: 35}} />
+      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
+      <Title />
     </svg>
   );
 };
@@ -39,4 +123,4 @@ Canvas.propTypes = {
   trackMouse: PropTypes.func.isRequired,
 };
 
-export default Canvas;
+export default Canvas;*/
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,59 @@
-import { MOVE_OBJECTS } from '../actions';
+import { MOVE_OBJECTS, START_GAME } from '../actions';
 import moveObjects from './moveObjects';
+import startGame from './startGame';
+
+const initialGameState = {
+  started: false,
+  kills: 0,
+  lives: 3,
+  flyingObjects: [],
+  lastObjectCreatedAt: new Date(),
+};
 
 const initialState = {
   angle: 45,
+  gameState: initialGameState,
 };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case MOVE_OBJECTS:
       return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
     default:
       return state;
   }
 }
 
 export default reducer;
+
+
+
+/*import { MOVE_OBJECTS, START_GAME } from '../actions';
+import moveObjects from './moveObjects';
+import startGame from './startGame';
+
+const initialGameState = {
+    started: false,
+    kills: 0,
+    lives: 3,
+  };
+
+const initialState = {
+  angle: 45,
+  gamestate: initialGameState,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS:
+      return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
+    default:
+      return state;
+  }
+}
+
+export default reducer;*/
